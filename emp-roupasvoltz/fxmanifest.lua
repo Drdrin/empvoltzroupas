1603acd6e2d3a271095459bc577a95aed253692d
volTzWRLD <82188352+volTzWRLD@users.noreply.github.com>
2021-08-04 13:58:35 -0300

    Add files via upload

---
+++ emp-roupasvoltz/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Voltz Resources'
+
+
+--SCRIPT DESENVOLVIDO POR voltz#1762
+
+--ENTRE EM MINHA LOJA! DISCORD: https://discord.gg/qarQmFYFnz
+
+--NÃO MUDE O NOME DO SCRIPT, SENÃO IRÁ PARAR DE FUNCIONAR!
+
+client_scripts {
+	'@vrp/lib/utils.lua',
+	'config2/configVOLTZ.lua',
+	'voltz-client.lua'
+}
+
+server_scripts {
+	'@vrp/lib/utils.lua',
+	'config2/configVOLTZ.lua',
+	'voltz-server.lua'
+}
+
